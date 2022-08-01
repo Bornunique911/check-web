9ab85da56d912fa7d29d46a789413f28453cbdec
Martin Peck <martin@meedan.com>
2022-08-01 13:55:14 -0400

    Create configs from ENV rather than using awscli to pull from SSM directly.

--- production/bin/create_configs.sh
+++ production/bin/create_configs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # This script generates json configuration files for check-web using values
-# from the SSM parameter store.
+# from the environment set via the SSM parameter store.
 
 # The following environment variables must be set:
 if [[ -z ${DEPLOY_ENV+x} || -z ${DEPLOYDIR+x} || -z ${AWS_DEFAULT_REGION+x} ]]; then
@@ -9,21 +9,20 @@ if [[ -z ${DEPLOY_ENV+x} || -z ${DEPLOYDIR+x} || -z ${AWS_DEFAULT_REGION+x} ]]; 
   exit 1
 fi
 
-SSM_PREFIX="/${DEPLOY_ENV}/check-web"
 WORKTMP=$(mktemp)
 
-# Create user config.js from SSM parameter value:
+# Create user config.js from ENV:
 DESTFILE="${DEPLOYDIR}/latest/config.js"
-aws ssm get-parameters --region $AWS_DEFAULT_REGION --name "${SSM_PREFIX}/config" | jq .Parameters[].Value|sed 's/["]//g' | python -m base64 -d > $WORKTMP
+echo $config | python -m base64 -d > $WORKTMP
 if (( $? != 0 )); then
   echo "Error retrieving SSM parameter ${SSM_PREFIX}/config. Exiting."
   exit 1
 fi
 mv $WORKTMP $DESTFILE
 
-# Create config-server.js from SSM parameter value:
+# Create config-server.js from ENV:
 DESTFILE="${DEPLOYDIR}/latest/config-server.js"
-aws ssm get-parameters --region $AWS_DEFAULT_REGION --name "${SSM_PREFIX}/config-server" | jq .Parameters[].Value|sed 's/["]//g' | python -m base64 -d > $WORKTMP
+echo $config_server | python -m base64 -d > $WORKTMP
 if (( $? != 0 )); then
   echo "Error retrieving SSM parameter ${SSM_PREFIX}/config-server. Exiting."
   exit 1
